85cfc882900a3141a8a12d55b24ec52a6b115e66
Frank Andersen <mail@frankandersen.dk>
2021-09-23 14:10:02 +0200

    docker script fix

--- docker_build_fa-scann.sh
+++ docker_build_fa-scann.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+sudo docker stop fa-scann
+printf "\n"
+sleep 3
+
+sudo docker container ls -a
+printf "\n"
+sleep 3
+
+sudo docker container rm fa-scann
+printf "\n"
+sleep 3
+
+sudo docker image ls -a
+printf "\n"
+sleep 3
+
+sudo docker image rm scann-fa-base
+printf "\n"
+sleep 3
 
 sudo docker build -t scann-fa-base .
-sudo docker run --name fa-scann -it scann-fa-base:latest /bin/bash 
+printf "\n"
+sleep 3
+
+sudo docker run --name fa-scann -it scann-fa-base:latest /bin/bash
+printf "\n"
+sleep 3
