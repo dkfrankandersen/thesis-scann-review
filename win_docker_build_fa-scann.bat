3938a3c8cd78886a797bacf1d457ab03836a4559
Frank Andersen <mail@frankandersen.dk>
2021-09-24 10:18:46 +0200

    powershell has issues with linebreak

--- win_docker_build_fa-scann.bat
+++ win_docker_build_fa-scann.bat
@@ -1,25 +1,19 @@
 docker stop fa-scann
-ECHO.
 TIMEOUT 3
 
 docker container ls -a
-ECHO.
 TIMEOUT 3
 
 docker container rm fa-scann
-ECHO.
 TIMEOUT 3
 
 docker image ls -a
-ECHO.
 TIMEOUT 3
 
 docker image rm scann-fa-base
-ECHO.
 TIMEOUT 3
 
 docker build -t scann-fa-base .
-ECHO.
 TIMEOUT 3
 
 docker run --name fa-scann -it scann-fa-base:latest /bin/bash
--- win_docker_connect_fa-scann.bat
+++ win_docker_connect_fa-scann.bat
@@ -1,15 +1,10 @@
 docker image ls -a
-ECHO.
 TIMEOUT 3
-
 
 docker container ls -a
-ECHO.
 TIMEOUT 3
 
-
 docker start fa-scann
-ECHO.
 TIMEOUT 3
 
 docker attach fa-scann 
