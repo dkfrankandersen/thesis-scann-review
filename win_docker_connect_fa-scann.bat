651b3e1594496bf08bb9bd532c0a4dc54c53536c
Frank Andersen <mail@frankandersen.dk>
2021-09-24 09:56:03 +0200

    docker bat script

--- win_docker_connect_fa-scann.bat
+++ win_docker_connect_fa-scann.bat
@@ -1,14 +1,15 @@
 docker image ls -a
-echo.
-sleep 3
+ECHO.
+TIMEOUT 3
+
 
 docker container ls -a
-echo.
-sleep 3
+ECHO.
+TIMEOUT 3
+
 
 docker start fa-scann
-echo.
-sleep 3
+ECHO.
+TIMEOUT 3
 
 docker attach fa-scann 
-sleep 3
